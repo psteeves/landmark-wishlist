a6f28d02097c5e92935d62702bf995e6c7b2559c
Pat Steeves <patrick.steeves@elementai.com>
2019-10-15 10:19:14 -0400

    support near and section queries

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from app.routes import api
+from flask_cors import CORS
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(api)
+    app.register_blueprint(api)
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request
+from app.utils import search_foursquare
+
+
+api = Blueprint("api", __name__)
+
+
+@api.route("/")
+def index():
+    return "<h1>Hello world</h1>"
+
+
+@api.route("/search_foursquare")
+def search():
+    near = request.args.get("near", "")
+    section = request.args.get("section", "")
+    results = search_foursquare(near, section)["response"]
+    return results
