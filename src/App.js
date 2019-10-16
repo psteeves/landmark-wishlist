535c277bbaf8c4f50c5137c2d5a2d2fa63c08752
Pat Steeves <patrick.steeves@elementai.com>
2019-10-15 23:19:03 -0400

    display search bar in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from "../public/favicon.jpeg"
 import './App.css';
+import { SearchBar } from "./components/SearchBar"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <div className="App-header">
           <img className="App-logo" src={logo} alt="logo" />
           <h2>Welcome to the Landmark WishList!</h2>
+        </div>
+        <div>
+          <SearchBar />
         </div>
       </div>
     );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+
+export class SearchBar extends React.Component {
+    render() {
+        return <TextField variant="outlined"  />;
+    }
+}
